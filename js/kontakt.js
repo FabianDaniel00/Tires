37c7137152dbec2c2fccaf03f6984a57c55343a1
Fábián Dániel <danielfabian090@gmail.com>
2021-11-09 15:36:19 +0100

    kontakt page finished

---
+++ js/kontakt.js
@@ -0,0 +1,3 @@
+const showPhoneNumber = (event) => {
+	event.target.children[0].innerText = "024 41 55 402";
+};
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,9 @@
 module.exports = {
-	purge:
-		// [],
-		{
-			enabled: true,
-			content: ["./**/*.html"],
-		},
+	purge: [],
+	// {
+	// 	enabled: true,
+	// 	content: ["./**/*.html"],
+	// },
 	// important: true,
 	darkMode: false, // or 'media' or 'class'
 	theme: {
